385cc33926eb303f920801429773f2fc9401938e
metatypeman <metatypeman@gmail.com>
2024-05-02 18:41:30 +0300

    TestSandBox: initial commit

---
+++ TestSandBox/Program.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+
+namespace TestSandBox
+{
+    internal class Program
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            _logger.Info("Hello, World!");
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            _logger.Info($"e.ExceptionObject = {e.ExceptionObject}");
+        }
+    }
+}
