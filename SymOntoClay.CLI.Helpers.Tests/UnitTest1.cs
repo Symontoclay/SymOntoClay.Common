e3e8028ead789e54378edc4ec205e838be27996a
metatypeman <metatypeman@gmail.com>
2024-05-11 10:04:09 +0300

    0.5.4 Test: initial commit

---
+++ SymOntoClay.CLI.Helpers.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SymOntoClay.CLI.Helpers.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
