9618f3e2963c31ebf9f0efedb4e932d19f14faab
metatypeman <metatypeman@gmail.com>
2024-05-15 20:19:13 +0300

    0.5.4 snapshot

--- TestSandBox/TstCommandLineParserHandlerWithNegativeCases.cs
+++ TestSandBox/TstCommandLineParserHandlerWithNegativeCases.cs
@@ -12,11 +12,62 @@ namespace TestSandBox
         {
             _logger.Info("Begin");
 
-            Case3_a();
+            Case4();
+            //Case3_a();
             //Case3();
             //Case2_a();
             //Case2();
             //Case1();
+
+            _logger.Info("End");
+        }
+
+        private void Case4()
+        {
+            _logger.Info("Begin");
+
+            try
+            {
+                var parser = new CommandLineParser(new List<BaseCommandLineArgument>()
+                {
+                    new CommandLineMutuallyExclusiveSet()
+                    {
+                        IsRequired = true,
+                        SubItems = new List<BaseCommandLineArgument>
+                        {
+                            new CommandLineArgument()
+                            {
+                                Name = "help",
+                                Aliases = new List<string>
+                                {
+                                    "h"
+                                },
+                                Kind = KindOfCommandLineArgument.Flag,
+                            },
+                            new CommandLineArgument()
+                            {
+                                Name = "run",
+                                Aliases = new List<string>
+                                {
+                                    "r"
+                                },
+                                Kind = KindOfCommandLineArgument.Flag,
+                            }
+                        }
+                    }
+                });
+
+                var args = new List<string>()
+                { "help", "run" };
+
+                var result = parser.Parse(args.ToArray());
+
+                _logger.Info($"result = {result}");
+            }
+            catch (Exception ex)
+            {
+                _logger.Info($"ex.Message = '{ex.Message}'");
+            }
 
             _logger.Info("End");
         }
