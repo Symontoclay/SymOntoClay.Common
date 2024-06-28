77e701c1893c1bf2d0f7bd8c0eb85c4d42417dd6
metatypeman <metatypeman@gmail.com>
2024-06-28 16:51:22 +0300

    0.5.5 updated common packages version

---
+++ SymOntoClay.Threading/DefaultCustomThreadPoolSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SymOntoClay.Threading
+{
+    public static class DefaultCustomThreadPoolSettings
+    {
+        public const int MinThreadsCount = 1;
+        public const int MaxThreadsCount = 10;
+    }
+}
