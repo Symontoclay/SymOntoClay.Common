23009b7fd674e1391215d2a730711a3e6adb74dc
metatypeman <metatypeman@gmail.com>
2026-05-01 10:35:39 +0300

    0.5.6 serialization refactoring

---
+++ SymOntoClay.Common/SerializationToImage/Attributes/SettingsMemberAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SymOntoClay.Common.SerializationToImage.Attributes
+{
+    public class SettingsMemberAttribute : Attribute
+    {
+    }
+}
---
+++ SymOntoClay.Common/SerializationToImage/Attributes/SystemNoSerializedMemberAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SymOntoClay.Common.SerializationToImage.Attributes
+{
+    public class SystemNoSerializedMemberAttribute : Attribute
+    {
+    }
+}
